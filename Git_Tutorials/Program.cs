0079848648a814d7c33619e7e69a4795d33d89f1
KingNathi <ngcamunkosinathi@gmail.com>
2021-01-21 18:28:48 +0200

    Add project files.

---
+++ Git_Tutorials/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
