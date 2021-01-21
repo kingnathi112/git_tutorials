4a6ffe3fa6b377b26b5a05a5d5f32b2de3ef2bab
Nkosinathi Mlotshwa <NkosinathiM@slingshotstudios.com>
2021-01-21 19:21:25 +0200

    merge

--- Git_Tutorials/Calculator.cs
+++ Git_Tutorials/Calculator.cs
@@ -2,10 +2,6 @@
 {
     public class Calculator
     {
-        public static int AddTwoNumbers(int num1, int num2)
-        {
-            return num1 + num2;
-        }
         public static int SubtractTwoNumbers(int num1, int num2)
         {
             return num1 - num2;
