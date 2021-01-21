4581cc3f34d8b15cb90ca2f5a8ad0c8bfdb847f3
Nkosinathi Mlotshwa <NkosinathiM@slingshotstudios.com>
2021-01-21 18:46:11 +0200

    Changed the Program.cs file

--- Git_Tutorials/Program.cs
+++ Git_Tutorials/Program.cs
@@ -6,7 +6,7 @@ namespace Git_Tutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a initial commit!");
         }
     }
 }
